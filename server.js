bda8123ba7c9c25918e134a31ed1db6b1666d4e5
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-02-06 21:19:17 -0500

    testing heroku

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use(express.static("public")); // Make public a static folder
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
-// mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
 
 app.get("/scrape", (req, res) => {
     // Here I will grab every information I need from whichever website I decide to scrape data from.
-    axios.get("http://techcrunch.com/europe/").then(function(response) {
+    axios.get("http://techcrunch.com/europe/").then(function (response) {
         const $ = cheerio.load(response.data);
 
-        $(".post-block").each(function(i, element) {
+        $(".post-block").each(function (i, element) {
             // Save an empty result object
             const result = {};
 
@@ -40,11 +40,11 @@ app.get("/scrape", (req, res) => {
             result.url = $(this).children("header").children("h2").children("a").attr("href");
 
             db.Article.create(result)
-                .then(function(dbArticle) {
+                .then(function (dbArticle) {
                     // View the added result in the console
                     console.log(dbArticle);
                 })
-                .catch(function(err) {
+                .catch(function (err) {
                     // If an error occurred, log it
                     console.log(err);
                 });
@@ -56,47 +56,47 @@ app.get("/scrape", (req, res) => {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
     db.Article.find({})
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
             // If we were able to successfully find Articles, send them back to the client
             res.json(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             // If an error occurred, send it to the client
             res.json(err);
         });
 });
 
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
         .populate("note")
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             // If an error occurred, send it to the client
             res.json(err);
         });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
-        .then(function(dbNote) {
+        .then(function (dbNote) {
             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
             // If we were able to successfully update an Article, send it back to the client
             res.json(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             // If an error occurred, send it to the client
             res.json(err);
         });
